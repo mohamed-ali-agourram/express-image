27204405914f6ce2796c92dce42886425048a4e3
mohamed-ali-agourram <mohamedaliagourram@gmail.com>
2024-03-21 21:42:35 +0000

    Add CRUD

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const express = require("express")
 const mongoose = require('mongoose');
 const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
 
+const postRouter = require("./routes/postRoute")
+
 const app = express()
+
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000
 
 const MONGO_URL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
 
+
 const connectWithRetry = () => {
 	mongoose.connect(MONGO_URL, {
 		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useFindAndModify: false
+		useUnifiedTopology: true
 	}).then(() => {
 		console.log("Connected to MongoDB")
 	}).catch((e) => {
@@ -20,26 +24,13 @@ const connectWithRetry = () => {
 		setTimeout(connectWithRetry, 5000)
 	})
 }
-
-// ensure mongo is up before connecting
 connectWithRetry()
-
-// mongoose.connect(
-// 	/*@mongo -> mongo is just like a domain name for the ip of the service (thats set by the use of express_default network that docker-composer sets up)*/
-// 	MONGO_URL,
-// 	{
-// 		useNewUrlParser: true,
-// 		useUnifiedTopology: true
-// 	}
-// ).then(() => {
-// 	console.log("Connected to MongoDB")
-// }).catch((err) => {
-// 	console.log("Error: ", err)
-// });
 
 app.get("/", (req, res) => {
 	res.send("<h1>Hello World!!!</h1>")
 })
+
+app.use("/posts", postRouter)
 
 app.listen(PORT, () => {
 	console.log(`http://localhost:${PORT}`)
